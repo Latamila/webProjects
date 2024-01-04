681e4ce624cc91063694f158b1429bb7eed88165
Latamila <87709951+Latamila@users.noreply.github.com>
2024-01-04 17:57:51 -0300

    Create app.py
    Cria automação para enviar mensagens automáticas no Whatsapp.

---
+++ app.py
@@ -0,0 +1,42 @@
+#pip install types-openpyxl
+#pip install pyautogui
+#pip install pillow 
+#pip install openpyxl
+
+
+
+import openpyxl
+from urllib.parse import quote
+import webbrowser
+from time import sleep
+import pyautogui
+
+
+
+webbrowser.open('https://web.whatsapp.com/',)
+sleep(30)
+workbook = openpyxl.load_workbook('clientes.xlsx')
+pagina_clientes = workbook['Planilha1']
+
+for linha in pagina_clientes.iter_rows(min_row=2):
+    nome = linha[0].value
+    telefone = linha[1].value
+    vencimento = linha[2].value
+
+    mensagem = f'Olá, {nome} to testando automação do envio de cobranças pelo whatsapp. seu boleto vence no dia {vencimento}. Evite multas e atrasos.'
+    
+    link_msg_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+    
+    webbrowser.open(link_msg_whatsapp)
+    sleep(10)
+    try:  
+        seta = pyautogui.locateCenterOnScreen('setaoficial.png')
+        sleep(5)
+        pyautogui.click(seta[0],seta[1])
+        sleep(5)
+        pyautogui.hotkey('ctrl','w')
+        sleep(5)
+    except:
+        print('Não foi possível enviar  mensagem para {nome}')
+        with open('erros.csv','a',newline='', encoding='utf-8') as arquivo:
+            arquivo.write(f'{nome}, {telefone}')
